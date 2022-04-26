4c77a95c3777f672ed9076ad0ca448918facee2d
veto221 <=>
2022-04-26 09:45:56 +0200

    review fixes

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ const Header = () => (
                             <div className="flex-shrink-0 flex grow md:grow-0 items-center justify-center">
                                 <Link href="/" passHref>
                                     <Image
-                                        className="block h-8 md:h-12 w-auto"
+                                        className="block h-8 md:h-12 w-auto cursor-pointer"
                                         src={Logo}
                                         alt="Flotiq"
                                         width="138px"
