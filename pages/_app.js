ff4fc1cf0ae233d84f0236b4255dee0b0662607f
Karol Grochowalski <karol.grochowalski@gmail.com>
2022-05-30 16:46:18 +0200

    GA tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,44 @@
 import '../styles/globals.css';
-import React from 'react';
+import { React, useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
-const MyApp = ({ Component, pageProps }) => (
-    <div>
-        <Component {...pageProps} />
-    </div>
-);
+const MyApp = ({ Component, pageProps }) => {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
+    return (
+        <div>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            {/* eslint-disable-next-line @next/next/inline-script-id */}
+            <Script
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+            />
+            <Component {...pageProps} />
+        </div>
+    );
+};
 
 export default MyApp;
