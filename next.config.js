7ca2a5118de00626dfa397a1be5363a92d126f34
Michał Chrząszcz <michal.chrzaszcz@codewave.pl>
2024-06-05 09:46:19 +0200

    Fix invalid next.config.js options detected issue

--- next.config.js
+++ next.config.js
@@ -1,42 +1,31 @@
 const withImages = require('next-images');
-const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(['flotiq-components-react']);
 
-module.exports = withPlugins(
-    [
-        withTM,
-        [
-            withImages,
+const nextConfig = {
+    async rewrites() {
+        return [
             {
-                exclude: /\.svg$/,
+                source: '/',
+                destination: '/1',
             },
-        ],
-    ],
-
-    {
-        async rewrites() {
-            return [
-                {
-                    source: '/',
-                    destination: '/1',
-                },
-            ];
-        },
-        images: {
-            dangerouslyAllowSVG: true,
-            disableStaticImages: true,
-            domains: ['api.flotiq.com'],
-        },
-        webpack: (config, options) => {
-            if (!options.isServer) {
-                config.resolve.alias['@sentry/node'] = '@sentry/browser';
-            }
-            config.module.rules.push({
-                test: /\.svg$/,
-                issuer: { and: [/\.(js|ts)x?$/] },
-                use: ['@svgr/webpack'],
-            });
-            return config;
-        },
+        ];
     },
-);
+    images: {
+        dangerouslyAllowSVG: true,
+        disableStaticImages: true,
+        domains: ['api.flotiq.com'],
+    },
+    webpack: (config, options) => {
+        if (!options.isServer) {
+            config.resolve.alias['@sentry/node'] = '@sentry/browser';
+        }
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: { and: [/\.(js|ts)x?$/] },
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
+
+module.exports = withTM(withImages(nextConfig));
